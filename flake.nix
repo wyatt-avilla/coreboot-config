482771c043b17b9276d6a52d44eceae144018193
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-06-20 15:31:02 -0700

    add devshell from wiki with modern-ish gnat

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "coreboot configuration";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells."x86_64-linux".default =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            gnat
+            ncurses
+            m4
+            flex
+            bison
+            zlib
+            pkg-config
+
+            qemu
+            flashrom
+          ];
+
+          shellHook = ''
+            # TODO remove?
+            NIX_LDFLAGS="$NIX_LDFLAGS -lncurses"
+          '';
+        };
+    };
+}
